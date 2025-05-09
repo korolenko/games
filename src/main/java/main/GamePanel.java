2bcf7df328f426eab8f81e9c2c0c61c9f415bd02
Roman Korolenko <korolenko.as1204@asugubkin.ru>
2025-05-09 22:00:30 +0300

    added objects

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import javax.swing.*;
@@ -32,7 +33,9 @@ public class GamePanel extends JPanel implements Runnable{
     Thread gameThread;
 
     public CollisionChecker checker = new CollisionChecker(this);
+    public AssetSetter assetSetter = new AssetSetter(this);
     public Player player = new Player(this,keyHandler);
+    public SuperObject obj[] = new SuperObject[10];
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -40,6 +43,10 @@ public class GamePanel extends JPanel implements Runnable{
         this.setDoubleBuffered(true);
         this.addKeyListener(keyHandler);
         this.setFocusable(true);
+    }
+
+    public void setupGame(){
+        assetSetter.setObject();
     }
 
     public void startGameThread(){
@@ -75,6 +82,13 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
         tileManager.draw(g2);
+
+        for(int i =0; i < obj.length; i++){
+            if(obj[i] != null){
+                obj[i].draw(g2,this);
+            }
+        }
+
         player.draw(g2);
         g2.dispose();
     }
---
+++ src/main/java/object/OBJ_key.java
@@ -0,0 +1,16 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_key extends SuperObject{
+    public OBJ_key(){
+        name = "key";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/key.png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
